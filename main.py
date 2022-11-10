77e1a1d1b9d005ae6cf8e84642aaa7c256b4b402
ceyo <37204646+A2ll@users.noreply.github.com>
2022-11-10 21:14:59 +0800

    V0.2
    更新“警告错误”

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from time import sleep
 import requests
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.firefox.options import Options as Firefox_Options
+from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
 from lxml import etree
 from loguru import logger
@@ -37,8 +39,13 @@ def picture_get(html_text):  # 通过网页的html代码获取到图片地址并
 
 
 def get_html_text(url):  # 获取人人文档网站html页面代码
-    binary = FirefoxBinary('D:\\Program Files\\Mozilla Firefox\\firefox.exe')  # 设置浏览器程序
-    driver = webdriver.Firefox(firefox_binary=binary)  # 设置Firefox浏览器
+    # binary = FirefoxBinary('D:\\Program Files\\Mozilla Firefox\\firefox.exe')  # 设置浏览器程序
+    # driver = webdriver.Firefox(firefox_binary=binary)  # 设置Firefox浏览器
+    # 上述设置方式被弃用，使用会有警告提示
+    firefox_options = Firefox_Options()
+    firefox_options.binary = r'D:\Program Files\Mozilla Firefox\firefox.exe'
+    s = Service(r"driver/geckodriver.exe")
+    driver = webdriver.Firefox(service=s, options=firefox_options)  # 指定 geckodirver 的位置
     driver.implicitly_wait(20)  # 隐形等待时间，等待网页加载完成
     driver.get(url)  # 打开url网页
     html = etree.HTML(driver.page_source)  # 用来解析字符串格式的HTML文档对象，将传进去的字符串转变成_Element对象。用于调用xpath()方法
@@ -54,9 +61,10 @@ def get_html_text(url):  # 获取人人文档网站html页面代码
         sleep(1)
         driver.execute_script("arguments[0].scrollIntoView();", view_xpath)  # 滑动至view_xpath中元素位置
         sleep(1)
-        driver.execute_script('window.scrollBy(0,-500)')  # 向上滚动浏览器500个像素数。
+        driver.execute_script('window.scrollBy(0,-800)')  # 向上滚动浏览器800个像素数。
         i = i + 1
-        sleep(random.uniform(0, 2))
+        sleep(random.uniform(5, 9))
+        driver.execute_script('window.scrollBy(0,300)')  # 向上滚动浏览器800个像素数。
     logger.info('加载完成，获取页面')
     html_str = driver.page_source
     get_html = "temp.html"
